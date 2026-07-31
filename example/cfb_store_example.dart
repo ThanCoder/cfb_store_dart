ea03e082d09f9ea75c5881dea2a76015477e0a3d
ThanCoder <thancoder2019@gmail.com>
2026-07-31 22:53:15 +0630

    forgot export events

--- example/cfb_store_example.dart
+++ example/cfb_store_example.dart
@@ -1,43 +1,22 @@
 // ignore_for_file: unused_local_variable
 
 import 'package:cfb_store/cfb_store.dart';
-import 'package:cfb_store/src/events/store_events.dart';
 
 void main() async {
   final store = CFBStore();
 
-  // event
-  store.events.listen((event) {
-    if (event is ValueLoaded) {
-      print('ValueLoaded');
-    }
-    if (event is ValueLoadError) {
-      print('ValueLoadError: ${event.message}');
-    }
-    if (event is ValueSaved) {
-      print('ValueSaved');
-    }
-    if (event is ValueSaveError) {
-      print('ValueSaveError: ${event.message}');
-    }
-    if (event is PutValue) {
-      print('PutValue Key: ${event.key}');
-    }
-  });
-  // if you not need stop listen event
-  store.close();
-
   await store.open('config.cfb');
   // store.openSync(dbPath)
 
-  store.put('name', 'ThanCoder');
-  store.put('age', 29);
-  store.put('is_femal', true);
-  store.put('list', ['one', 'two', 'three']);
-  store.put('map-list', [
-    {'name': 'thancoder', 'age': 29},
-  ]);
-  store.put('map', {'name': 'thancoder', 'age': 28, 'height': 5.6});
+  final ml = <Map<String, dynamic>>[
+    {'name': 'than'},
+    {'name': 'coder'},
+    {
+      'map-list': {'sub': 'subname'},
+    },
+  ].toList();
+
+  await store.putAndWriteAll('map-list', ml);
 
   //save disk
   await store.writeAll();
--- lib/cfb_store.dart
+++ lib/cfb_store.dart
@@ -1,3 +1,4 @@
 library;
 
 export 'src/cfb_store_base.dart';
+export 'src/events/store_events.dart';
